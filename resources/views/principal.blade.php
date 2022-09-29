63a461a591828feddc2f0a928641131a7127cbd4
ingvalencia <ing.gvalenci@gmail.com>
2022-09-29 13:51:32 -0500

    Creando un Layout principal.

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,10 @@
+{{--Utilizando directivas--}}
+@extends('layouts.app')
+
+@section('titulo')
+    Página Principal
+@endsection
+
+@section('contenido')
+    Aquí el contendio de principal
+@endsection
