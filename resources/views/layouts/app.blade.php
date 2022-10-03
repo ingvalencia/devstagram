3a0b0ce9e6b461581b0f18848fea4be8c72be3d1
ingvalencia <ing.gvalenci@gmail.com>
2022-10-03 14:33:00 -0500

    Se instalo Tailwind CSS y se empezo con la creacion de la navegacion y estilos para el Layout Principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,40 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Devstagram - @yield('titulo')</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
 
     <body>
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda">Tienda</a>
-        </nav>
 
-        <h1>@yield('titulo')</h1>
-        <hr>
-        @yield('contenido')
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                    DevStagram
+                </h1>
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear cuenta</a>
+                </nav>
+            </div>
+        </header>
+        {{--Contendido de las vistas--}}
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center  p-5 text-gray-500 font-bold text-1xl mb-10">
+            Desarrollado por Giovanny Valencia - Todos los derechos reservados
+            @php
+                echo date('Y')
+            @endphp
+
+        </footer>
+
+
 
     </body>
 
